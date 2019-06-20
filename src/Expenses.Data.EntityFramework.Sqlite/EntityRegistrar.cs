6ddb7cb6998749a890e7ba96d48b47e5629a6e0e
afandylamusu <afandy.lamusu@live.co.uk>
2019-06-20 09:32:46 +0700

    Initial

---
+++ src/Expenses.Data.EntityFramework.Sqlite/EntityRegistrar.cs
@@ -0,0 +1,23 @@
+﻿// Copyright © 2017 Dmitry Sikorsky. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Expenses.Data.Entities;
+using ExtCore.Data.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expenses.Data.EntityFramework.Sqlite
+{
+  public class EntityRegistrar : IEntityRegistrar
+  {
+    public void RegisterEntities(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Expense>(etb =>
+        {
+          etb.HasKey(e => e.Id);
+          etb.Property(e => e.Id).ValueGeneratedOnAdd();
+          etb.ToTable("Expenses");
+        }
+      );
+    }
+  }
+}
---
+++ src/Incomes.Data.EntityFramework.Sqlite/EntityRegistrar.cs
@@ -0,0 +1,23 @@
+﻿// Copyright © 2017 Dmitry Sikorsky. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using ExtCore.Data.EntityFramework;
+using Incomes.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Incomes.Data.EntityFramework.Sqlite
+{
+  public class EntityRegistrar : IEntityRegistrar
+  {
+    public void RegisterEntities(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Income>(etb =>
+        {
+          etb.HasKey(e => e.Id);
+          etb.Property(e => e.Id).ValueGeneratedOnAdd();
+          etb.ToTable("Incomes");
+        }
+      );
+    }
+  }
+}
