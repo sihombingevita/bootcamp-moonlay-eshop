3382c13dfc910293fc4c6d5abfe91f6a35530e80
afandylamusu <afandy.lamusu@live.co.uk>
2019-06-21 11:33:33 +0700

    Add Core Data projects

---
+++ src/Data.Abstractions/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using ExtCore.Data.Abstractions;
+using System;
+using System.Collections.Generic;
+
+namespace Data.Abstractions
+{
+    public interface IRepository<TEntity> : IRepository
+    {
+        TEntity WithKey(int id);
+
+        IEnumerable<TEntity> All(int page = 0, int size = 25);
+
+        void Create(TEntity entity);
+
+        void Edit(TEntity entity);
+
+        void Delete(int id);
+
+        void Delete(TEntity entity);
+    }
+}
---
+++ src/Data.Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using ExtCore.Data.Entities.Abstractions;
+using System;
+
+namespace Data.Entities
+{
+    public class Entity : IEntity
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset Created { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTimeOffset Modified { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
